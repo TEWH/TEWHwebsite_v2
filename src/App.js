8a6fe2ee4d23bade5c69cb675fd3482e4c83c176
Abhi <aramani35@gmail.com>
2019-04-13 16:52:05 -0500

    added pretty navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import RealNavBar from './components/RealNavbar'
+import 'bootstrap/dist/css/bootstrap.css'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <RealNavBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
