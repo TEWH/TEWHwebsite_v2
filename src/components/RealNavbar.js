60bcdea3b6cc3cb47a20427b147cf0dfa40afac4
Abhi <aramani35@gmail.com>
2019-04-13 17:52:52 -0500

    added fonts + edited nav

--- src/components/RealNavbar.js
+++ src/components/RealNavbar.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown'; 
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './css/RealNavBar.css' 
 
 class RealNavBar extends Component {
     render() {
       return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand href="#home">
+          <img src={require('./img/tewh.png')}  />
+           | Texas Engineering World Health
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+        <Nav.Link href="#features">Projects</Nav.Link>
+        <Nav.Link href="#pricing">Member Resources</Nav.Link>
+        <Nav.Link href="#pricing">Supporters</Nav.Link>
         </Nav>
         <Nav>
-        <Nav.Link href="#deets">More deets</Nav.Link>
-        <Nav.Link eventKey={2} href="#memes">
+        <Nav.Link href="#deets">Contact</Nav.Link>
+        {/* <Nav.Link eventKey={2} href="#memes">
             Dank memes
-        </Nav.Link>
+        </Nav.Link> */}
         </Nav>
     </Navbar.Collapse>
     </Navbar>
